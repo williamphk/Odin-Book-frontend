36067e54145254026d565146c52b4750c9a2cda8
William Poon <willphk@outlook.com>
2023-05-07 15:33:39 -0400

    Revised LeftSidebar

--- src/components/layout/LeftSidebar.js
+++ src/components/layout/LeftSidebar.js
@@ -1,7 +1,10 @@
-// LeftSidebar.js
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const LeftSidebar = ({ className }) => {
+  const friends = useSelector((state) => state.page.friends);
+
   return (
     <div className={className}>
       <div className="flex items-center mb-4">
@@ -15,16 +18,39 @@ const LeftSidebar = ({ className }) => {
       <nav>
         <ul className="space-y-2">
           <li className="flex items-center space-x-2">
-            <i className="text-xl"></i>
-            <span className="text-gray-600">Home</span>
+            <button className="text-gray-600">Home</button>
+          </li>
+          <li className="flex flex-col items-start space-x-2">
+            <button className="text-gray-600">Friends</button>
+            {friends && (
+              <ul className="flex flex-col items-start">
+                <li>
+                  <Link to="friends/">Friend requests</Link>
+                </li>
+                <li>
+                  <Link to="friends/suggestions">Friend suggestions</Link>
+                </li>
+                <li>All firends</li>
+              </ul>
+            )}
           </li>
           <li className="flex items-center space-x-2">
-            <i className="text-xl"></i>
-            <span className="text-gray-600">Friends</span>
+            <button className="text-gray-600">Ad Center</button>
           </li>
           <li className="flex items-center space-x-2">
-            <i className="text-xl"></i>
-            <span className="text-gray-600">Watch</span>
+            <button className="text-gray-600">Most Recent</button>
+          </li>
+          <li className="flex items-center space-x-2">
+            <button className="text-gray-600">Groups</button>
+          </li>
+          <li className="flex items-center space-x-2">
+            <button className="text-gray-600">Marketplace</button>
+          </li>
+          <li className="flex items-center space-x-2">
+            <button className="text-gray-600">Watch</button>
+          </li>
+          <li className="flex items-center space-x-2">
+            <button className="text-gray-600">See more</button>
           </li>
         </ul>
       </nav>
