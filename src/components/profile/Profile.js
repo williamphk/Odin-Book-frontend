20d048cb2c21115b35513630a02d49388932ca62
William Poon <willphk@outlook.com>
2023-05-09 23:40:59 -0400

    Improved Profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,32 +6,28 @@ import MaterialIcon from "../common/MaterialIcon";
 const Profile = () => {
   const user = useSelector((state) => state.auth.user);
 
-  const handleProfilePicClick = () => {};
-
   return (
     <div className="flex flex-col w-full text-left">
       <div className="flex justify-between w-full bg-white mt-48 relative h-48 pr-60">
         <div className="flex items-center">
           <div className="absolute bottom-12 left-60">
             <div className="flex relative">
-              <button onClick={handleProfilePicClick}>
-                <img
-                  src={user.picture}
-                  alt="Profile"
-                  className="object-cover w-48 h-48 rounded-full border-4 border-white"
-                />
-              </button>
-              <div className="w-8 h-8 rounded-full bg-gray-200 flex justify-center items-center absolute right-3 bottom-3">
+              <img
+                src={user.picture}
+                alt="Profile"
+                className="object-cover w-48 h-48 rounded-full border-4 border-white"
+              />
+              <button className="w-8 h-8 rounded-full bg-gray-200 hover:bg-gray-300 flex justify-center items-center absolute right-3 bottom-3">
                 <MaterialIcon
                   className="material-symbols-outlined text-xl text-black"
                   iconName={"photo_camera"}
                 />
-              </div>
+              </button>
             </div>
           </div>
           <div className="pl-[450px]">
             <h2 className="flex text-3xl font-medium">{user.fullName}</h2>
-            <div>1 friend</div>
+            <div className="text-gray-500">1 friend</div>
           </div>
         </div>
         <div className="flex items-center">
@@ -47,8 +43,22 @@ const Profile = () => {
         </div>
       </div>
       <div className="flex px-32 gap-x-4 px-60 pt-6">
-        <div className="w-[40%] bg-white rounded shadow">
-          <div>Intro</div>
+        <div className="w-[40%]">
+          <div className="w-full bg-white rounded shadow p-3 flex flex-col gap-y-4">
+            <div>Intro</div>
+            <div className="text-sm rounded bg-gray-200 hover:bg-gray-300 py-1">
+              <button className="w-full">Add work</button>
+            </div>
+            <div className="text-sm rounded bg-gray-200 hover:bg-gray-300 py-1">
+              <button className="w-full">Add eduation</button>
+            </div>
+            <div className="text-sm rounded bg-gray-200 hover:bg-gray-300 py-1">
+              <button className="w-full">Add current city</button>
+            </div>
+            <div className="text-sm rounded bg-gray-200 hover:bg-gray-300 py-1">
+              <button className="w-full">Edit details</button>
+            </div>
+          </div>
           <div>Friends</div>
         </div>
         <div className="w-[60%] bg-white rounded shadow">Posts</div>
