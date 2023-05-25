6e44eae5f93d8db1f98c2e749bc46372af1c8320
William Poon <willphk@outlook.com>
2023-05-24 22:45:01 -0400

    Refactored Delete Friend Button

---
+++ src/components/friends/DeleteButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { deleteFriend } from "../../api";
+import { incrementDeleteCount } from "../../slices/userSlice";
+
+const DeleteButton = ({ friend }) => {
+  const token = useSelector((state) => state.auth.token);
+  const user = useSelector((state) => state.auth.user);
+
+  const dispatch = useDispatch();
+
+  const handleDeleteButton = async () => {
+    await deleteFriend(token, user._id, friend.user);
+    dispatch(incrementDeleteCount());
+  };
+
+  return (
+    <button
+      onClick={handleDeleteButton}
+      className="bg-gray-100 hover:bg-red-500 hover:text-white text-gray-500 rounded-lg p-2 w-[200px] h-[35px] overflow-hidden flex justify-center items-center transition duration-200"
+    >
+      Delete
+    </button>
+  );
+};
+
+export default DeleteButton;
--- src/components/friends/Friend.js
+++ src/components/friends/Friend.js
@@ -1,22 +1,10 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
 
-import { deleteFriend } from "../../api";
-import { incrementDeleteCount } from "../../slices/userSlice";
 import ProfilePic from "../common/ProfilePic";
 import UserName from "../common/UserName";
+import DeleteButton from "./DeleteButton";
 
 const Friend = ({ friend }) => {
-  const token = useSelector((state) => state.auth.token);
-  const user = useSelector((state) => state.auth.user);
-
-  const dispatch = useDispatch();
-
-  const handleDeleteButton = async () => {
-    await deleteFriend(token, user._id, friend.user);
-    dispatch(incrementDeleteCount());
-  };
-
   return (
     <div className="shadow-sm flex flex-col bg-white rounded-lg w-[220px] pb-3 items-center justify-center">
       <div className="flex flex-col w-full h-[230px] overflow-hidden">
@@ -33,12 +21,7 @@ const Friend = ({ friend }) => {
           className="my-2 hover:underline"
         />
       </div>
-      <button
-        onClick={handleDeleteButton}
-        className="bg-gray-100 hover:bg-red-500 hover:text-white text-gray-500 rounded-lg p-2 w-[200px] h-[35px] overflow-hidden flex justify-center items-center transition duration-200"
-      >
-        Delete
-      </button>
+      <DeleteButton friend={friend} />
     </div>
   );
 };
