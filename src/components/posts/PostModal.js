62574a0a7ba66d752456474d1cb8ce9f643a2eff
William Poon <willphk@outlook.com>
2023-05-27 13:22:06 -0400

    Redesign PostModal

--- src/components/posts/PostModal.js
+++ src/components/posts/PostModal.js
@@ -1,9 +1,11 @@
 import React from "react";
-import "../styles.css";
 import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
 
+import "../styles.css";
 import MaterialIcon from "../common/MaterialIcon";
 import InputField from "../common/InputField";
+import ProfilePic from "../common/ProfilePic";
 
 const PostModal = ({
   title,
@@ -24,26 +26,47 @@ const PostModal = ({
     handleSubmit,
     setError,
     formState: { errors },
+    watch,
   } = useForm();
 
   const closeIcon = "close";
 
+  const user = useSelector((state) => state.auth.user);
+
+  const allFieldValues = watch();
+
+  //console.log(!!allFieldValues.content);
+
   // if (!isOpen) return null;
 
   return (
-    <div className="add-post-modal">
-      <div className="modal-overlay" onClick={closePostModal}></div>
-      <div className="modal-content">
-        <div className="flex items-center justify-between mb-4">
-          <h2 className="text-xl font-bold">{title}</h2>
-          <button onClick={closePostModal}>
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+      <div
+        className="absolute w-full h-full bg-gray-100 bg-opacity-80"
+        onClick={closePostModal}
+      ></div>
+      <div className="relative bg-white rounded-lg z-50 w-[36%] shadow-3xl">
+        <div className="flex items-center justify-center border-b-[1px]">
+          <h2 className="text-xl font-bold py-4">{title}</h2>
+          <button
+            onClick={closePostModal}
+            className="w-[38px] h-[38px] hover:bg-gray-200 rounded-full flex justify-center items-center absolute right-3 top-3"
+          >
             <MaterialIcon
-              className="material-symbols-outlined text-4xl hover:bg-gray-100 rounded-lg"
+              className="material-symbols-outlined text-3xl text-gray-600"
               iconName={closeIcon}
             />
           </button>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex mx-4 my-4 items-center">
+          <ProfilePic
+            picture={user.picture}
+            id={user._id}
+            className="object-cover w-10 h-10 rounded-full"
+          />
+          <div className="ml-2">{user.fullName}</div>
+        </div>
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
           {requiredInputField && (
             <InputField
               register={register}
@@ -53,9 +76,10 @@ const PostModal = ({
               placeholder={placeholder}
               value={value}
               setPostContent={setPostContent}
-              rows={4}
+              rows={6}
               isTextArea={true}
               labeltext="Post content"
+              inputClassName="mx-4 text-xl focus:outline-none"
               validation={{
                 required: "Content is required",
               }}
@@ -72,12 +96,13 @@ const PostModal = ({
           <button
             className={`${
               buttonColor || "bg-purple-500"
-            } text-white px-4 py-2 rounded-md shadow-md mt-6 w-20 ${
+            } text-white px-4 py-2 rounded-md m-4 ${
               buttonHoverColor
                 ? "hover:" + buttonHoverColor
                 : "hover:bg-purple-600"
             }
-            transition duration-200`}
+            transition duration-200 disabled:bg-gray-300 disabled:text-gray-400`}
+            disabled={!allFieldValues.content}
           >
             {button}
           </button>
